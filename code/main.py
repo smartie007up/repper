217f7d41a2ff9fa786deb897cbab72c2260baf49
pim0223 <pim.sauter@gmail.com>
2016-06-02 15:42:10 +0200

    shit is gefixt

--- code/main.py
+++ code/main.py
@@ -1,21 +1,17 @@
-with open('salami_chords.txt', 'r') as f:
+# Read in file from McGill dataset
+with open('../dataset/0003/salami_chords.txt', 'r') as f:
     data = f.readlines()
 
-# an array for all the lines (rows)
-L = [data]
-# size of the amount of lines (rows)
-n = len(L)
+data = data[6:len(data)]
+print(data)
 
-for line in data:
-    words = line.split(' | ')
 
-# total size of the amount of words on each line
-R = []
-R.append(words)
-k = len(R)
+data[1] = data[1].split('|')
+data[1] = data[1][1:len(data[1])-1]
 
-# usable data starts on line 7 (in all salami_chords.txt files)
-for x in L[7:n]:
-    # from column 2 to length of line
-    for y in R[2:k]:
-        print x, y
+
+# # usable data starts on line 7 (in all salami_chords.txt files)
+# for x in [7:n]:
+#     # from column 2 to length of line
+#     for y in R[2:k]:
+#         print x, y
