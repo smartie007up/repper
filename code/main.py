572ee9a4feab5e4c1f602367f576ef5d7d86ca3c
smartie007up <Sinaasappel1>
2016-06-16 13:19:46 +0200

    deletie

--- code/main.py
+++ code/main.py
@@ -3,15 +3,24 @@ with open('../dataset/0003/salami_chords.txt', 'r') as f:
     data = f.readlines()
 
 data = data[6:len(data)]
-print(data)
+
+S = []
+
+for i in range(0, len(data)-1):
+    d = data[i].split("\t")
+    d = d[1].split('|')
+    d = d[0:len(d)-1]
+    S.append(d)
+    i += 1
+
+# data = data[1].split('|')
+# data = data[0:len(data)-1]
 
 
-data[1] = data[1].split('|')
-data[1] = data[1][1:len(data[1])-1]
+# for i in range(0, len(data)-1):
+#     data[i] = data[i].split("\t")for i in range(0, len(data)-1):
+#
+# for j in range(0, len(data-1))
+#     data[j] = data[1].split('|')
 
-
-# # usable data starts on line 7 (in all salami_chords.txt files)
-# for x in [7:n]:
-#     # from column 2 to length of line
-#     for y in R[2:k]:
-#         print x, y
+print(d)
